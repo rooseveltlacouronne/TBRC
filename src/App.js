aeeada1d26a40a75dbe4eb4f47888465f0a42da8
1COURONNE <rdivin1950@Gmail.com>
2025-07-07 02:55:36 +0100

    Fix Netlify config: removed homepage, added _redirects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { Navbar } from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Certifications } from "./components/Certifications";
+import { Services } from "./components/Services";
+import { Gallery } from "./components/Gallery";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#0a0a0a] text-white overflow-x-hidden">
+      <Navbar /> 
+      <Home /> 
+      <About /> 
+      <Skills />
+      <Projects />
+      <Certifications />
+      <Services />
+      <Gallery />
+      <Contact />
+      <Footer /> 
     </div>
   );
 }
